19a410c891e88b64225508a270bdbce3c0f646a8
Bharat <119512639+Bharatjawa2@users.noreply.github.com>
2024-03-13 22:59:55 +0530

    Add files via upload

---
+++ calculator-app-part1-boilerplate-master/test/calculatorApp.spec.js
@@ -0,0 +1,32 @@
+const chai = require('chai');
+const expect = chai.expect;
+const { addition, subtraction } = require('../src/calculatorApp');
+
+describe('Addition Functionality', () => {
+  
+  it('Check for addition of two positive numbers and return the sum as positive number', () => {
+    // Write Test Case Here
+  });
+
+  it('Check for addition of two negative numbers and return the sum as negative number.', () => {
+    // Write Test Case Here
+  });
+
+  it('Check if either of number is negative produce subtracted output.', () => {
+    // Write Test Case Here
+  });
+
+
+});
+
+describe('Subtraction Functionality', () => {
+  it('Check for subtracting two positive number and return positive subtraction', () => {
+    // Write Test Case Here
+  });
+  it('Check if either of number is negative produce sum as output', () => {
+    // Write Test Case Here
+  });
+  it('Subtracting zero will produce zero as subtraction.', () => {
+    // Write Test Case Here
+  });
+});
